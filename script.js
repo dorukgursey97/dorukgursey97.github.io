05a2a60543a11bf1157c1cdd0b2fc31697e4d6d9
dorukgursey97 <dorukgursey@gmail.com>
2026-03-04 19:54:13 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Initialize Icons
+lucide.createIcons();
+
+// Simple smooth scroll for links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
